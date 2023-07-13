995cacae4bccddd7ea38160203b637063eda0cbe
walidata48 <136953190+walidata48@users.noreply.github.com>
2023-07-13 09:40:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from dash import Dash, html, Input, Output,dcc,dash_table
 import dash_bootstrap_components as dbc
 import plotly.express as px
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], 
-                meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}", }],
-                )
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], )
 
 #Load Image
 image_path = 'assets/survey.jpg'
